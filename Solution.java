4bbd2202bbc2211606f6b0f543f9746d00f3313c
SOURAV RAY <souravray288@gmail.com>
2025-12-18 20:21:28 +0530

    Implement sorting of odd and even numbers in array
    The method sorts an array by separating odd and even numbers, sorting them in reverse and natural order respectively, and then merging them back.

---
+++ Solution.java
@@ -0,0 +1,31 @@
+class Solution {
+    public void sortIt(int[] arr) {
+        List<Integer> odd = new ArrayList<>();
+        List<Integer> even = new ArrayList<>();
+        
+        for (int x : arr) {
+            if ((x & 1) == 1) odd.add(x);
+            else even.add(x);
+        }
+        
+        odd.sort(Collections.reverseOrder());
+        Collections.sort(even);
+        
+        int idx = 0;
+        for (int x : odd) arr[idx++] = x;
+        for (int x : even) arr[idx++] = x;
+    }
+}
+/* Time & Space Complexity
+Time: O(n log n)
+
+Space: O(n) */
+
+
+
+
+
+
+
+
+
